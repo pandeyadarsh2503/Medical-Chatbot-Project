ef17c02d20853b9e46349f2aef13ff2cf445dcd6
Adarsh Pandey <pandeyadarsh2503@gmail.com>
2026-01-31 12:52:57 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical-Chatbot-Project",
+    version="1.0.0",
+    author="Adarsh Pandey",
+    author_email="paneyadarsh2503@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
